c24b922bf712a982757a3866b462e18e096620b5
daydreamboy <wesley4chen@gmail.com>
2021-10-17 17:15:07 +0800

    add webapp

--- Example/IOPatch/WCViewController.m
+++ Example/IOPatch/WCViewController.m
@@ -7,6 +7,11 @@
 //
 
 #import "WCViewController.h"
+#import <TBWeiwo/TBWeiwo.h>
+#import <TBWeiwo/TBWeiwo+Internal.h>
+//#import "TBWeiwo+Internal.h"
+
+#define STR_OF_JSON(...) @#__VA_ARGS__
 
 @interface WCViewController ()
 
@@ -17,7 +22,15 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+//    NSString *JSONString = STR_OF_JSON(
+//       {"args":[[{"type":"raw","value":{"name":"UIApplication"}},{"type":"raw","value":{"name":"sharedApplication"}}]],"methodName":"call","target":{"body":[{"name":"setSlot","args":[[{"literal":"jscodes"}],[{"name":"squareBrackets"}]]},{"name":";"},{"name":"setSlot","args":[[{"literal":"blockSpec"}],[{"name":"curlyBrackets","args":[[{"literal":"body"},{"name":":","args":[[{"name":"body"}]]}],[{"literal":"signature"},{"name":":","args":[[{"literal":"v@"}]]}],[{"literal":"options"},{"name":":","args":[[{"name":"curlyBrackets","args":[[{"literal":"on_global"},{"name":":","args":[[{"literal":true}]]}],[{"name":"jscodes"}]]}]]}]]}]]},{"name":";"},{"name":"$"},{"name":"createBlock:","args":[[{"name":"blockSpec"}]]},{"name":"call"},{"name":";"},{"name":"return","args":[[{"name":"jscodes"},{"name":"componentsJoinedByString:","args":[[{"literal":";"}]]}]]}],"paramNames":["body"],"signature":"@@@","type":"block","name":"doCode"},"containerAsValue":true}
+//    );
+//    
+//    id JSONObject = [NSJSONSerialization JSONObjectWithData:[JSONString dataUsingEncoding:NSUTF8StringEncoding] options:kNilOptions error:nil];
+//    
+//    [[TBWeiwo sharedInstance] _callWithParams:JSONObject];
 }
 
 - (void)didReceiveMemoryWarning
